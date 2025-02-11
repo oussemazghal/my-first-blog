d2884226f5d693b18199760e4b0f4a8c4903aba6
oussema <oussemazghal@gmail.com>
2025-02-11 15:20:01 +0100

    Ajout de la vue et du template de détail blog post ainsi que du CSS pour le site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views  # Importation des vues
+from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),  # Page d'accueil du blog
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
